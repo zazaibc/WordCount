6b06be66942ed7d47e9a7b02dbcce92520ae2272
zz <3056426520@qq.com>
2024-05-18 18:37:06 +0800

    update3

--- WordCount/WordCount.cpp
+++ WordCount/WordCount.cpp
@@ -1,20 +1,55 @@
-﻿// WordCount.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 
-#include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+// 函数用于统计字符数
+int countCharacters(const string& filename) {
+    ifstream file(filename);
+    char c;
+    int count = 0;
+    while (file.get(c)) {
+        count++;
+    }
+    return count;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
+// 函数用于统计单词数
+int countWords(const string& filename) {
+    ifstream file(filename);
+    string word;
+    int count = 0;
+    while (file >> word) {
+        count++;
+    }
+    return count;
+}
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        cout << "Usage: " << argv[0] << " [-c | -w] [input_file_name]" << endl;
+        return 1;
+    }
+
+    string parameter = argv[1];
+    string filename = argv[2];
+
+    int count = 0;
+
+    if (parameter == "-c") {
+        count = countCharacters(filename);
+        cout << "字符数：" << count << endl;
+    }
+    else if (parameter == "-w") {
+        count = countWords(filename);
+        cout << "单词数：" << count << endl;
+    }
+    else {
+        cout << "Invalid parameter. Please use -c or -w." << endl;
+        return 1;
+    }
+
+    return 0;
+}
